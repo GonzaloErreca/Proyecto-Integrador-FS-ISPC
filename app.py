2be53d576956665d6cf04a8c659b4254f2db18cb
Gonzalo Erreca <gonzaerreca@gmail.com>
2022-11-21 22:29:20 -0300

    enrutamiento de vistas protegidas

--- app.py
+++ app.py
@@ -264,3 +264,25 @@ def storage():
     db.connection.commit()
 
     return redirect('/home')
+
+
+@app.route('/protected')
+@login_required
+def protected():
+    return "<h1>Esta es una vista protegida, solo para usuarios autenticados.</h1>"
+
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>", 404
+
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    csrf.init_app(app)
+    app.register_error_handler(401, status_401)
+    app.register_error_handler(404, status_404)
+    app.run()   
